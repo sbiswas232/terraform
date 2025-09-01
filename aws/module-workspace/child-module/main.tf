356dd5597593dde6285e44d6c63bc79141512a8a
sbiswas232 <139542832+sbiswas232@users.noreply.github.com>
2025-09-01 21:37:52 +0530

    Add files via upload

--- aws/module-workspace/child-module/main.tf
+++ aws/module-workspace/child-module/main.tf
@@ -1,6 +1,7 @@
 # Create VPC
 resource "aws_vpc" "vpc" {
   cidr_block           = var.vpc_cidr
+  instance_tenancy     = "default"
   enable_dns_hostnames = true
   enable_dns_support   = true
   tags = {
