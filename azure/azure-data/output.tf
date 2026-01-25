29f17b7cbfa02b07941a29bfb79e6fe409cc9d71
sbiswas232 <139542832+sbiswas232@users.noreply.github.com>
2026-01-26 02:08:14 +0530

    Create output.tf

---
+++ azure/azure-data/output.tf
@@ -0,0 +1,11 @@
+output "resource-group-id" {
+  value = data.azurerm_resource_group.project25-01.id
+}
+
+output "resource-group-name" {
+  value = data.azurerm_resource_group.project25-01.name
+}
+
+output "resource-group-location" {
+  value = data.azurerm_resource_group.project25-01.location
+}
