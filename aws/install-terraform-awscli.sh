dae3afbf3ec2947d5b3ed351791a595650220b31
sbiswas232 <139542832+sbiswas232@users.noreply.github.com>
2026-01-29 11:47:48 +0530

    Create install-terraform-awscli.sh

---
+++ aws/install-terraform-awscli.sh
@@ -0,0 +1,33 @@
+# nano install-terraform-awscli.sh 
+
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+
+gpg --no-default-keyring \
+--keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+--fingerprint
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+sudo apt update
+
+sudo apt-get install terraform -y
+
+# Install AWS CLI in Ubuntu:
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+
+apt install zip -y
+
+unzip awscliv2.zip
+
+./aws/install 
+
+terraform --version
+
+aws --version
